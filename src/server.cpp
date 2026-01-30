ef18a23cdfacd15253b827002a45567389da524b
Artyom <artem.portnov2014@gmail.com>
2026-01-30 16:19:29 +0000

    fixes

--- src/server.cpp
+++ src/server.cpp
@@ -1,22 +1,41 @@
 #include <arpa/inet.h>
 #include <asm-generic/socket.h>
 #include <cerrno>
+#include <chrono>
 #include <cstddef>
 #include <cstdio>
 #include <cstring>
+#include <ctime>
 #include <errno.h>
 #include <fcntl.h>
 #include <iostream>
+#include <mutex>
 #include <netinet/in.h>
-#include <string>
+
 #include <sys/epoll.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <thread>
 #include <time.h>
+#include <tuple>
 #include <unistd.h>
 #include <unordered_map>
 
 #include <format>
+#include <utility>
+
+using Clock = std::chrono::steady_clock;
+using TimePoint = std::chrono::time_point<Clock>;
+
+std::atomic<bool> server_running{true};
+
+// Perfomance Parameters
+const int MAX_ITERATION_SERVER_ACCEPT = 64;
+const int MAX_ITERATION_CLIENT = 5;
+const int MAX_EVENTS = 128;
+const int BUF_SIZE = 1024;
+const size_t MAX_MSG_SIZE = BUF_SIZE - sizeof(uint32_t) * 2;
+const int MAX_CLIENTS = 10000;
 
 struct MessageHeader {
   uint32_t type;
@@ -25,19 +44,25 @@ struct MessageHeader {
 
 struct ClientInfo {
   int fd;
-  time_t last_activ;
-  char buff[1024];
-  size_t recived;
-  bool header_recived;
+  TimePoint last_activ;
+  char buff[BUF_SIZE];
+  size_t recived = 0;
+  bool header_recived = false;
   MessageHeader header;
+
+  ClientInfo(int f) : fd(f), last_activ(Clock::now()) {}
 };
 
 struct Stats {
-  int active_clients = 0;
-  int total_received = 0;
-  int total_sent = 0;
-  int errors = 0;
+  std::atomic<int> active_clients{0};
+  std::atomic<int> total_received{0};
+  std::atomic<int> total_sent{0};
+  std::atomic<int> errors{0};
 } stats;
+
+// Globals
+std::unordered_map<int, ClientInfo> clients;
+std::mutex clients_mutex;
 
 int make_socket_non_block(int fd) {
   int flags = fcntl(fd, F_GETFL, 0);
@@ -47,16 +72,153 @@ int make_socket_non_block(int fd) {
   return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
 }
 
-int close_client_connection(int epoll_fd, int client_fd,
-                             std::unordered_map<int, ClientInfo> &clients) {
+void remove_client_locked(int epoll_fd, int client_fd) {
   epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client_fd, nullptr);
   close(client_fd);
-  std::cout << std::format("Client disconnected: fd={}",
-                           std::to_string(client_fd))
-            << std::endl;
-  auto it = clients.erase(client_fd);
+  clients.erase(client_fd);
   stats.active_clients--;
-  return it;
+}
+
+void remove_client(int epoll_fd, int client_fd) {
+  std::lock_guard<std::mutex> lock(clients_mutex);
+  remove_client_locked(epoll_fd, client_fd);
+}
+
+void process_client_messages(int epoll_fd, ClientInfo &cl) {
+  int count = 0;
+  while (count <= MAX_ITERATION_CLIENT) {
+    if (!cl.header_recived) {
+      if (cl.recived < sizeof(MessageHeader))
+        break;
+      memcpy(&cl.header, cl.buff, sizeof(MessageHeader));
+      cl.header_recived = true;
+    }
+
+    if (cl.header.size > MAX_MSG_SIZE) {
+      std::cerr << "Message too large" << cl.fd << std::endl;
+      remove_client(epoll_fd, cl.fd);
+      return;
+    }
+
+    size_t full_msg_size = sizeof(MessageHeader) + cl.header.size;
+    if (cl.recived < full_msg_size)
+      break;
+
+    // send back
+    send(cl.fd, cl.buff, full_msg_size, 0);
+    stats.total_received++;
+    stats.total_sent++;
+
+    size_t remaining = cl.recived - full_msg_size;
+    if (remaining > 0) {
+      memmove(cl.buff, cl.buff + full_msg_size, remaining);
+    }
+    cl.recived = remaining;
+    cl.header_recived = false;
+
+    count += 1;
+  }
+}
+
+void handle_client_data(int epoll_fd, int fd, TimePoint now) {
+  std::lock_guard<std::mutex> lock(clients_mutex);
+
+  auto it = clients.find(fd);
+  if (it == clients.end())
+    return;
+
+  ClientInfo &cl = it->second;
+  cl.last_activ = now;
+
+  // В режиме EPOLLET нужно читать до EAGAIN
+  while (true) {
+    ssize_t n = recv(fd, cl.buff + cl.recived, BUF_SIZE - cl.recived, 0);
+
+    if (n < 0) {
+      if (errno == EAGAIN || errno == EWOULDBLOCK)
+        break;
+      stats.errors++;
+
+      remove_client_locked(epoll_fd, fd);
+      return;
+    }
+
+    if (n == 0) {
+
+      remove_client_locked(epoll_fd, fd);
+      return;
+    }
+
+    cl.recived += n;
+    process_client_messages(epoll_fd, cl);
+
+    if (cl.recived >= BUF_SIZE) {
+      std::cerr << "Buffer overflow, closing " << fd << std::endl;
+
+      remove_client_locked(epoll_fd, fd);
+      return;
+    }
+  }
+}
+void handle_new_conn(int epoll_fd, int listen_sock) {
+  while (true) {
+    struct sockaddr_in addr;
+    socklen_t len = sizeof(addr);
+    int client_fd = accept(listen_sock, (struct sockaddr *)&addr, &len);
+
+    if (client_fd < 0) {
+      if (errno != EAGAIN && errno != EWOULDBLOCK)
+        perror("accept");
+      break;
+    }
+
+    if (stats.active_clients >= MAX_CLIENTS) {
+      close(client_fd);
+      continue;
+    }
+
+    make_socket_non_block(client_fd);
+    epoll_event ev;
+    ev.events = EPOLLIN || EPOLLET;
+    ev.data.fd = client_fd;
+    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &ev);
+
+    {
+      std::lock_guard<std::mutex> lock(clients_mutex);
+      clients.emplace(std::piecewise_construct,
+                      std::forward_as_tuple(client_fd),
+                      std::forward_as_tuple(client_fd));
+      stats.active_clients++;
+    }
+    std::cout << "New Client... " << client_fd << std::endl;
+  }
+}
+
+void cleaner_thread(int epoll_fd) {
+  while (server_running) {
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    auto now = Clock::now();
+    std::vector<int> to_remove;
+
+    {
+      std::lock_guard<std::mutex> lock(clients_mutex);
+      for (auto const &[fd, cl] : clients) {
+        if (now - cl.last_activ > std::chrono::seconds(30)) {
+          to_remove.push_back(fd);
+        }
+      }
+    }
+
+    for (int fd : to_remove) {
+      std::cout << "Timeout: " << fd << std::endl;
+      remove_client(epoll_fd, fd);
+    }
+
+    std::cout << std::format("Stats: Active={}, Sent={}, Recv={}, Errors={} \n",
+                             stats.active_clients.load(),
+                             stats.total_sent.load(),
+                             stats.total_received.load(), stats.errors.load());
+  }
 }
 
 int main(int argc, char **argv) {
@@ -67,7 +229,6 @@ int main(int argc, char **argv) {
   }
 
   int port = atoi(argv[1]);
-  const uint16_t MAX_CLIENTS = 10000;
 
   // 1 socket
   int sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -112,161 +273,39 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  // bind listen sock to epoll
   struct epoll_event ev;
   ev.events = EPOLLIN;
   ev.data.fd = sock;
   epoll_ctl(epoll_fd, EPOLL_CTL_ADD, sock, &ev);
 
-  std::unordered_map<int, ClientInfo> clients;
-  struct epoll_event events[100];
-  time_t last_stats = time(nullptr);
+  std::thread cleaner(cleaner_thread, epoll_fd);
 
   std::cout << "Server listening on port " << port << std::endl;
+  epoll_event events[MAX_EVENTS];
 
   // Event loop
+  while (server_running) {
+    int n = epoll_wait(epoll_fd, events, MAX_EVENTS, 1000);
+    auto now = Clock::now();
 
-  while (true) {
-    int n = epoll_wait(epoll_fd, events, 100, 1000);
-
-    time_t now = time(nullptr);
-
-    if (now - last_stats >= 1) {
-      std::cout << "Active clients: " << stats.active_clients
-                << ", Total received: " << stats.total_received
-                << ", Total sent: " << stats.total_sent
-                << ", Errors: " << stats.errors << std::endl;
-      last_stats = now;
-    }
-
-    for (auto it = clients.begin(); it != clients.end();) {
-      if (now - it->second.last_activ > 30) {
-        std::cout << std::format("Timeout client fd: {}",
-                                 std::to_string(it->first))
-                  << std::endl;
-        stats.errors++;
-        int fd_to_close = it->first;
-
-        epoll_ctl(epoll_fd, EPOLL_CTL_DEL, it->first, nullptr);
-        close(it->first);
-        it = clients.erase(it);
-        stats.active_clients--;
-        stats.errors++;
-        
-
-      } else {
-        ++it;
-      }
-    }
-
+    // Iterate events (MAX_EVENTS)
     for (int i = 0; i < n; i++) {
-      int fd = events[i].data.fd;
-
-      if (fd == sock) {
-
-        struct sockaddr_in client_addr;
-        socklen_t len = sizeof(client_addr);
-
-        int client_fd = accept(sock, (struct sockaddr *)&client_addr, &len);
-
-        if (client_fd < 0) {
-          break;
-        }
-        if (stats.active_clients >= MAX_CLIENTS) {
-          std::cerr << "Max clients reached, rejecting new connection"
-                    << std::endl;
-          close(client_fd);
-          stats.errors++;
-          continue;
-        }
-
-        make_socket_non_block(client_fd);
-
-        // add to epoll
-        ev.events = EPOLLIN | EPOLLET;
-        ev.data.fd = client_fd;
-        epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &ev);
-
-        ClientInfo cl_info;
-        cl_info.fd = client_fd;
-        cl_info.last_activ = now;
-        cl_info.recived = 0;
-        cl_info.header_recived = false;
-
-        clients[client_fd] = cl_info;
-        stats.active_clients++;
-
-        std::cout << std::format("New client connected: fd={}",
-                                 std::to_string(client_fd))
-                  << std::endl;
+      if (events[i].data.fd == sock) {
+        handle_new_conn(epoll_fd, sock);
       } else {
-        ClientInfo &cl_info = clients[fd];
-        cl_info.last_activ = now;
-
-        while (true) {
-          ssize_t count = recv(fd, cl_info.buff + cl_info.recived,
-                               sizeof(cl_info.buff) - cl_info.recived, 0);
-
-          if (count < 0) {
-            if (errno == EAGAIN || errno == EWOULDBLOCK) {
-              break;
-            }
-            perror("recv");
-            stats.errors++;
-            close_client_connection(epoll_fd, fd, clients);
-            break;
-          }
-          if (count == 0) {
-            close_client_connection(epoll_fd, fd, clients);
-            break;
-          }
-
-          cl_info.recived += count;
-
-          while (true) {
-            if (!cl_info.header_recived) {
-              if (cl_info.recived < sizeof(MessageHeader)) {
-                break;
-              }
-              memcpy(&cl_info.header, cl_info.buff, sizeof(MessageHeader));
-              cl_info.header_recived = true;
-            }
-
-            if (cl_info.header.size > 1016) {
-              std::cerr << std::format("Message too large from client fd:{}",
-                                       fd)
-                        << std::endl;
-              stats.errors++;
-              close_client_connection(epoll_fd, fd, clients);
-              goto next_event;
-            }
-
-            // wait for full message
-            size_t msg_size = sizeof(MessageHeader) + cl_info.header.size;
-            if (cl_info.recived < msg_size) {
-              break;
-            }
-
-            // read full message
-            ssize_t sent = send(fd, cl_info.buff, msg_size, 0);
-            if (sent != (ssize_t)msg_size) {
-              stats.errors++;
-              close_client_connection(epoll_fd, fd, clients);
-              goto next_event;
-            }
-            stats.total_received++;
-            stats.total_sent++;
-
-            // remove message from buffer
-            size_t remaining = cl_info.recived - msg_size;
-            cl_info.recived = remaining;
-            memmove(cl_info.buff, cl_info.buff + msg_size, remaining);
-            cl_info.header_recived = false;
-          }
-        }
+        handle_client_data(epoll_fd, events[i].data.fd, now);
       }
-    next_event:;
     }
-  };
+  }
+
+  std::cout << "Shutting down server...";
+  server_running = false;
+  cleaner.join();
+
+  for (auto &[fd, _] : clients) {
+    close(fd);
+  }
 
   close(sock);
   close(epoll_fd);
